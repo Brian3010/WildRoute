771c68fafe9a6acbfcafc4c373fed92b98e2dd6b
Brian <briannguyenwg@gmail.com>
2023-06-22 15:46:11 +1000

    initial setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; //https://www.youtube.com/watch?v=Ul3y1LXxzdU
 import App from './App.tsx';
 
-import './index.css';
+import './assets/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
