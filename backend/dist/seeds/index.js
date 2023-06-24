0247fef138fd4129f04b4b340fc3bb29c52bbee8
Brian <briannguyenwg@gmail.com>
2023-06-25 00:30:27 +1000

    install mongoose types - no error for dup monoose

--- backend/dist/seeds/index.js
+++ backend/dist/seeds/index.js
@@ -1,9 +1,11 @@
 "use strict";
-// import mongoose from 'mongoose';
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 main().catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wildRoute');
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    await mongoose_1.default.connect('mongodb://127.0.0.1:27017/wildRoute');
 }
 //# sourceMappingURL=index.js.map
