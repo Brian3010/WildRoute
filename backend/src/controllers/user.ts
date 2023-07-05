3925c859afa30c62fef425c74ae5f42c5c97db2b
Brian <briannguyenwg@gmail.com>
2023-07-05 22:50:11 +1000

    add register user

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,23 @@
 import { RequestHandler } from 'express';
+import User from '../models/user';
+// export const index: RequestHandler = (req, res, next) => {
+//   res.send('index from user');
+// };
 
-export const index: RequestHandler = (req, res, next) => {
-  res.send('index from user');
+type UserBody = {
+  user: {
+    username: string;
+    password: string;
+    email: string;
+  };
+};
+
+export const registerUser: RequestHandler<unknown, unknown, UserBody, unknown> = async (req, res, next) => {
+  const { email, username, password } = req.body.user;
+  // save to the dbs
+  const user = new User({ email, username });
+  // use passport to register new user
+  const newUser = await User.register(user, password);
+
+  // todo: refer to <https://github.com/Brian3010/YelpCamp_pracs/blob/master/controllers/users.js>
 };
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,6 @@ import passport from 'passport';
 import * as userController from '../controllers/user';
 const router: Router = express.Router();
 
-router.route('/').get(passport.authenticate('local'), userController.index);
+router.route('/').get(userController.registerUser);
 
 export default router;
