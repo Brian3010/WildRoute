d125618208e2c5fecbc53676e4e1cea40c04bb03
Brian <briannguyenwg@gmail.com>
2023-08-02 18:07:58 +1000

    add pagination

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,9 +1,23 @@
 import { Pagination } from '@mui/material';
+import { ChangeEvent } from 'react';
 
-export default function AppPagination() {
+interface AppPaginationProps {
+  actiesPerPage: number;
+  totalActies: number;
+  pageOnchange: (pageNum: number) => void;
+}
+
+export default function AppPagination({ actiesPerPage, totalActies, pageOnchange }: AppPaginationProps) {
+  const numOfpages = Math.ceil(totalActies / actiesPerPage);
+
+  const handleOnChange: (env: ChangeEvent<unknown>, page: number) => void = (env, page) => {
+    env.preventDefault();
+    pageOnchange(page);
+  };
+
   return (
     <>
-      <Pagination count={10} variant="outlined" color="secondary" />
+      <Pagination count={numOfpages} variant="outlined" color="secondary" onChange={handleOnChange} />
     </>
   );
 }
--- frontend/src/pages/ActivityList.tsx
+++ frontend/src/pages/ActivityList.tsx
@@ -1,5 +1,5 @@
 import { Box, Grid } from '@mui/material';
-
+import { useState } from 'react';
 import ActivityItem from '../components/ActivityItem';
 import AppPagination from '../components/Pagination';
 
@@ -78,12 +78,25 @@ const sampleData: ActyDataList = [
 ];
 
 function ActivityList() {
+  // activities per page
+  const [actiesPerPage] = useState(4);
+  // current page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // get current activities
+  const indexOfLastActy = currentPage * actiesPerPage; // 1 * 3 = [3] // 2 * 3 = [6]
+  const indexOfFirstActy = indexOfLastActy - actiesPerPage; // 3 - 3 = [0] // 6 - 3 = [3]
+  const currentActies = sampleData.slice(indexOfFirstActy, indexOfLastActy);
+
+  // change page
+  const paginationOnChange: (pageNum: number) => void = pageNum => setCurrentPage(pageNum);
+
   return (
     <>
       <h1>Acitivity List Page</h1>
 
       <Grid container spacing={2}>
-        {sampleData.map((el, i) => (
+        {currentActies.map((el, i) => (
           <Grid key={i} item xs={12} sm={6} md={4} lg={3}>
             <ActivityItem data={el} />
           </Grid>
@@ -91,7 +104,11 @@ function ActivityList() {
       </Grid>
 
       <Box display={'flex'} justifyContent={'center'} marginTop={2} marginBottom={2}>
-        <AppPagination />
+        <AppPagination
+          actiesPerPage={actiesPerPage}
+          totalActies={sampleData.length}
+          pageOnchange={paginationOnChange}
+        />
       </Box>
     </>
   );
