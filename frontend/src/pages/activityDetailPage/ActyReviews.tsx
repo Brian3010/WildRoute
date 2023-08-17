96eae75338a8a1eedc8a753b63ea434c367c3863
Brian <briannguyenwg@gmail.com>
2023-08-17 17:31:27 +1000

    minor change on username review

--- frontend/src/pages/activityDetailPage/ActyReviews.tsx
+++ frontend/src/pages/activityDetailPage/ActyReviews.tsx
@@ -22,7 +22,7 @@ export default function ActyReviews(props: ActyReviewProps) {
       {currentReviews.map(r => {
         return (
           <Paper key={r._id} variant="outlined" sx={{ padding: '16px 24px 12px', marginBottom: '8px' }}>
-            <Typography variant="subtitle1" fontWeight={700}>
+            <Typography variant="subtitle1" fontWeight={600}>
               {r.owner.username}
             </Typography>
             <Rating sx={{ paddingTop: '8px' }} name="read-only" value={r.rating} readOnly />
