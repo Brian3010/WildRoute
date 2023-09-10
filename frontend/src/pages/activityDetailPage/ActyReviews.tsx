d9b60528c7e976bde64108a5e63392128271a0c4
Brian <briannguyenwg@gmail.com>
2023-09-10 16:34:10 +1000

    workin on removing review

--- frontend/src/pages/activityDetailPage/ActyReviews.tsx
+++ frontend/src/pages/activityDetailPage/ActyReviews.tsx
@@ -1,7 +1,9 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 import { Box, Chip, Pagination, Paper, Rating, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import useAxiosInterceptor from '../../hooks/useAxiosInterceptor';
 import { TActyDetail } from '../../services/getActyById';
 
@@ -10,28 +12,37 @@ interface ActyReviewProps {
 }
 
 export default function ActyReviews(props: ActyReviewProps) {
+  const { id: actyId } = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
   const axiosInterceptor = useAxiosInterceptor();
+  const { auth } = useAuth();
 
   const reviewPerPage = 6;
   const reviews = props.reviews.slice().reverse();
 
-  const [currentPage, setCurrentPage] = useState(1);
   const numOfpages = Math.ceil(reviews.length / reviewPerPage);
 
   const lastReviewIndex = currentPage * reviewPerPage;
   const firstReviewIndex = lastReviewIndex - reviewPerPage;
   const currentReviews = reviews.slice(firstReviewIndex, lastReviewIndex);
 
-  const deleteReview = async (actyId: string, reviewId: string, accessToken: string) => {
-    const res = await axiosInterceptor.delete(`/activities/${actyId}/review/${reviewId}`, {
-      headers: { Authorization: `bearer ${accessToken}` },
-    });
+  // useEffect(() => {
+  //   const deleteReview = async (actyId: string, reviewId: string, accessToken: string) => {
+  //     const res = await axiosInterceptor.delete(`/activities/${actyId}/review/${reviewId}`, {
+  //       headers: { Authorization: `bearer ${accessToken}` },
+  //     });
 
-    return res;
-  };
+  //     return res;
+  //   };
 
-  const handleRemoveClick = () => {
+  // },[])
+
+  const handleRemoveClick = (reviewId: string, reviewOwner: string) => {
     console.log('handleRemoveClick clicked');
+
+    const loginUser = auth.user._id;
+    //check isLoggedIn and owner of the review, disable and enable button based on that.
+    console.log({ actyId, reviewId, reviewOwner, loginUser });
   };
 
   return (
@@ -46,15 +57,15 @@ export default function ActyReviews(props: ActyReviewProps) {
                 // sx={{fontSize:'0.8rem'} }
                 sx={{ display: { xs: 'none', sm: 'inherit' }, fontSize: '0.75rem' }}
                 label="Remove"
-                onClick={handleRemoveClick}
-                icon={<DeleteIcon sx={{ fontSize: '0.9rem' }} />}
+                onClick={() => handleRemoveClick(r._id, r.owner._id)}
+                icon={<DeleteIcon sx={{ fontSize: '1rem' }} />}
                 variant="outlined"
                 color="error"
               />
               <DeleteRoundedIcon
-                onClick={handleRemoveClick}
+                onClick={() => handleRemoveClick(r._id, r.owner._id)}
                 color="error"
-                sx={{ display: { xs: 'inherit', sm: 'none' }, cursor: 'pointer' }}
+                sx={{ display: { xs: 'inherit', sm: 'none' }, cursor: 'pointer', fontSize: '1.3rem' }}
               />
             </Typography>
             <Rating sx={{ paddingTop: '8px' }} name="read-only" value={r.rating} readOnly />
