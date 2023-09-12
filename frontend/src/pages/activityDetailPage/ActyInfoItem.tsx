765efee8b16f121e916b55da4e50c412de48319a
Brian <briannguyenwg@gmail.com>
2023-09-13 00:49:14 +1000

    create DropDownMenu component

--- frontend/src/pages/activityDetailPage/ActyInfoItem.tsx
+++ frontend/src/pages/activityDetailPage/ActyInfoItem.tsx
@@ -1,28 +1,11 @@
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import {
-  Box,
-  Card,
-  CardContent,
-  IconButton,
-  Menu,
-  MenuItem,
-  Rating,
-  Snackbar,
-  Tooltip,
-  Typography,
-} from '@mui/material';
-import { useState } from 'react';
-import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
+import { Box, Card, CardContent, Rating, Typography } from '@mui/material';
+
 import '../../assets/ActivityInfoItem.css';
-import useAuth from '../../hooks/useAuth';
-import useAxiosInterceptor from '../../hooks/useAxiosInterceptor';
-import useFlashMessage from '../../hooks/useFlashMessage';
+
 import iconSrcs from '../../images';
 import { TActyDetail } from '../../services/getActyById';
 import DipslayTagIcons from './DisplayTagIcons';
+import DropDownMenu from './DropDownMenu';
 
 interface ActyInfoItemProps {
   actyDetail: Omit<TActyDetail, 'image' | 'reviews'>;
@@ -31,18 +14,6 @@ interface ActyInfoItemProps {
 
 export default function ActyInfoItem({ actyDetail, reviewTotal }: ActyInfoItemProps) {
   // console.log('ActyInfoItem rendered');
-  const { id: actyId } = useParams();
-  const navigate = useNavigate();
-  const { auth } = useAuth();
-  // const isLoggedIn = auth.accessToken.length > 0;
-  const axiosInterceptor = useAxiosInterceptor();
-  const { setFlashMessage } = useFlashMessage();
-
-  const isOwnner = auth.user._id === actyDetail.author._id;
-
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const [snackBarOpen, setSnackBarOpen] = useState(false);
 
   // convert the tags to icons
   const convertToIcon = (tags: typeof actyDetail.tags) => {
@@ -69,52 +40,6 @@ export default function ActyInfoItem({ actyDetail, reviewTotal }: ActyInfoItemPr
   const actyTags = convertToIcon(actyDetail.tags);
   // console.log(actyTags);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // console.log('dropdown menu clicked');
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-    setSnackBarOpen(false);
-  };
-
-  const handleEdit = () => {
-    // setAnchorEl(null);
-
-    console.log('directing to edit page');
-
-    // return <Navigate to={`/activities/${actyId}/edit`} />
-    return navigate(`/activities/${actyId}/edit`, { replace: true });
-  };
-
-  const handleCopyLink = async () => {
-    // setAnchorEl(null);
-
-    try {
-      await navigator.clipboard.writeText(window.location.href);
-      setSnackBarOpen(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleDelete = async () => {
-    console.log('handle Delete Clicked');
-
-    try {
-      const res = await axiosInterceptor.delete(`/activities/${actyId}`, {
-        withCredentials: true,
-        headers: { Authorization: `bearer ${auth.accessToken}` },
-      });
-      // console.log(res);
-      setFlashMessage({ message: `Successfully deleted ${res.data.activity_title} `, type: 'success' });
-      return navigate('/activities', { state: { openFlashMsg: true } });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <Card sx={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 6px 0px', height: '100%' }}>
       <CardContent sx={{ padding: 2, height: '100%', position: 'relative' }}>
@@ -129,59 +54,7 @@ export default function ActyInfoItem({ actyDetail, reviewTotal }: ActyInfoItemPr
             </Typography>
 
             {/* dropdown menu */}
-
-            <IconButton
-              aria-label="modify activity"
-              id="modify-button"
-              aria-controls={open ? 'modify-menu' : undefined}
-              aria-expanded={open ? 'true' : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-              sx={{ paddingX: '2px', paddingY: '2px', maxHeight: '32px' }}
-            >
-              <MoreHorizIcon sx={{ fontSize: '1.8rem' }} />
-            </IconButton>
-            <Menu
-              id="modify-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'modify-button',
-              }}
-            >
-              <Tooltip title={isOwnner ? '' : 'Only the owner can do this'} placement="top-start">
-                <span>
-                  <MenuItem sx={{ gap: 1.5 }} onClick={handleEdit} disabled={isOwnner ? false : true}>
-                    <EditIcon fontSize="small" />
-                    {/* <Link style={{ textDecoration: 'none', color: 'inherit' }} to={`/activities/${actyId}/edit`}>
-                  Edit
-                </Link> */}
-                    <Typography sx={{ textDecoration: 'none', color: 'inherit' }}>Edit</Typography>
-                  </MenuItem>
-
-                  <MenuItem
-                    sx={{ gap: 1.5, color: '#d32f2f' }}
-                    onClick={handleDelete}
-                    disabled={isOwnner ? false : true}
-                  >
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                  </MenuItem>
-                </span>
-              </Tooltip>
-              <MenuItem sx={{ gap: 1.5 }} onClick={handleCopyLink}>
-                <ContentCopyIcon fontSize="small" />
-                Copy link
-              </MenuItem>
-            </Menu>
-            <Snackbar
-              open={snackBarOpen}
-              onClose={handleClose}
-              message="Link copied"
-              // key={}
-            />
-
+            <DropDownMenu authorId={actyDetail.author._id} />
             {/* ------------------ */}
           </Box>
 
