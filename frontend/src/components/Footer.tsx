f48670354dc3af22d5eca41495a1dfad1a80d8fa
Brian <briannguyenwg@gmail.com>
2023-12-09 19:59:39 +1100

    style footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import '../assets/Footer.css';
+import socialMediaIcons from '../images/socialMediaIcons';
 
 function Footer() {
   return (
@@ -8,29 +10,50 @@ function Footer() {
       sx={{
         marginTop: 'auto',
         padding: '10px',
-        textAlign: 'center',
-        background: '#121212',
+        // textAlign: 'center',
+        backgroundColor: '#121212',
         color: '#fff',
       }}
     >
-      <Typography
-        variant="h6"
+      <Box
         sx={{
-          my: 2,
-          fontFamily: 'monospace',
-          fontWeight: 700,
-          letterSpacing: '.1rem',
+          display: 'flex',
+          justifyContent: { xs: 'center', sm: 'space-between' },
+          padding: '0 50px 0 50px',
+          flexWrap: 'wrap',
         }}
       >
-        WildRoute
-      </Typography>
-      <Box>Social Medias here</Box>
-      <Typography variant="overline">&copy; 2023 Brian Nguyen. All rights reserved.</Typography>
+        <Box>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+            }}
+          >
+            WildRoute
+          </Typography>
+          <Box component={'div'} className="social-media-container">
+            <a href="https://github.com/Brian3010" target="_blank">
+              <img src={socialMediaIcons.GitHubIcon} alt="GitHub icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/brian-nguyen-411483196" target="_blank">
+              <img src={socialMediaIcons.LinkedInIcon} alt="Linkedin icon" />
+            </a>
+            <a href="mailto:briannguyenwg@gmail.com" >
+              <img src={socialMediaIcons.GmailIcon} alt="Gmail icon" />
+            </a>
+          </Box>
+        </Box>
+        <Typography alignSelf="flex-end" variant="overline">
+          &copy; 2023 Brian Nguyen. All rights reserved.
+        </Typography>
+      </Box>
     </Box>
   );
 }
 
 export default Footer;
-//TODO: Fix footer to be at the bottom where it belongs
 //TODO: add social medias
 //TODO: add background and stuff for the homepage
---
+++ frontend/src/images/socialMediaIcons.ts
@@ -0,0 +1,5 @@
+import GitHubIcon from './github-icon.png';
+import GmailIcon from './gmail-icon.png';
+import LinkedInIcon from './linkedin-icon.png';
+
+export default { GmailIcon, GitHubIcon, LinkedInIcon };
