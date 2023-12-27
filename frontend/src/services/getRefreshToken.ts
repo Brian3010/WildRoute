0a0ebd2270c5e135f1f2fb147fcc778d190f4de7
Brian <briannguyenwg@gmail.com>
2023-12-27 16:16:23 +1100

    minor change on request url

--- frontend/src/services/getRefreshToken.ts
+++ frontend/src/services/getRefreshToken.ts
@@ -2,11 +2,9 @@ import axios from './axios';
 
 type TgetRefreshToken = (userId: string, username: string) => Promise<{ accessToken: string }>;
 
-
-
-const getRefreshToken: TgetRefreshToken = async (userId , username) => {
+const getRefreshToken: TgetRefreshToken = async (userId, username) => {
   const res = await axios.post<{ accessToken: string }>(
-    '/user/refresh-token',
+    '/user/refresh-token/',
     { _id: userId, username },
     { withCredentials: true }
   );
