70dd9cb9afa05663f7e50b56126815bc85bac0aa
Brian <briannguyenwg@gmail.com>
2023-12-27 15:38:10 +1100

    change base url

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = 'https://wildroute-api.onrender.com' ;
-export const BACKEND_URL = 'http://localhost:3000';
+export const BACKEND_URL = 'https://wildroute-api.onrender.com';
+// export const BACKEND_URL = 'http://localhost:3000';
